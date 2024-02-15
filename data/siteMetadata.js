0cd868d4a6b08477bae2dfc4aa349233c2cc1176
Hoang Long <70263014+hoanglong2311@users.noreply.github.com>
2024-02-15 10:15:01 +0700

    Update title website ⚒️

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,8 +2,8 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Long Nguyen',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'Better Engineer',
+  description: 'A blog to share technical knowledge and lifestyle',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
